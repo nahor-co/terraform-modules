6d25b0b5f8ecf22351c99fa063ac6ad4da44b152
Bob Rohan <bob.rohan@nahor.co.uk>
2025-01-07 13:48:28 +0000

    add s3 public access block

--- aws/static_website/s3.tf
+++ aws/static_website/s3.tf
@@ -23,8 +23,16 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "instance" {
 
 }
 
-resource "aws_s3_bucket_policy" "instance" {
+resource "aws_s3_bucket_public_access_block" "instance" {
+  bucket = aws_s3_bucket.instance.id
 
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "instance" {
   bucket = aws_s3_bucket.instance.id
   
   policy = jsonencode(
